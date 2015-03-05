b5b0d6d802d2b441d151b7f2049799f0b3352cd7
Myrmidon010 <sebcruz@outlook.com>
2015-03-05 14:33:00 -0800

    Completed Assignment Post
    Associated scripts and png of the required plots

---
+++ plot2.R
@@ -0,0 +1,24 @@
+# Original Source Data as Zip
+https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# Note: Unzip and place in your working directory.  All references below assume the present working directory is set, that
+# the data has been unzipped and placed in that directory.
+
+# Import Data
+hpc_raw = read.table("hpc.txt", header = TRUE, sep= ";", na.strings = "?", stringsAsFactors=F)
+
+# Exclude NULL records
+hpc = na.omit(hpc_raw)
+
+# Append Calculated Date/Time Field to hpc
+hpc$DateTime = as.POSIXct(paste(hpc$Date, hpc$Time, sep=" "), format="%d/%m/%Y %H:%M:%S")
+
+# Convert Date field to Date datatype
+hpc$Date = as.Date(hpc$Date, format="%d/%m/%Y")
+
+# Subset hpc
+hpc_sub = hpc[hpc$Date >= "2007-02-01" & hpc$Date <= "2007-02-02", ]
+
+# Line Graph
+png("plot2.png")
+plot(hpc_sub$DateTime, as.numeric(hpc_sub$Global_active_power) ,type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,35 @@
+# Original Source Data as Zip
+https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# Note: Unzip and place in your working directory.  All references below assume the present working directory is set, that
+# the data has been unzipped and placed in that directory.
+
+# Import Data
+hpc_raw = read.table("hpc.txt", header = TRUE, sep= ";", na.strings = "?", stringsAsFactors=F)
+
+# Exclude NULL records
+hpc = na.omit(hpc_raw)
+
+# Append Calculated Date/Time Field to hpc
+hpc$DateTime = as.POSIXct(paste(hpc$Date, hpc$Time, sep=" "), format="%d/%m/%Y %H:%M:%S")
+
+# Convert Date field to Date datatype
+hpc$Date = as.Date(hpc$Date, format="%d/%m/%Y")
+
+# Subset hpc
+hpc_sub = hpc[hpc$Date >= "2007-02-01" & hpc$Date <= "2007-02-02", ]
+
+# Colored Line Graph with Legend
+png("plot3.png")
+plot(hpc_sub$DateTime, as.numeric(hpc_sub$Sub_metering_1), col="black", type="n", xlab="", ylab="Energy Sub metering")
+lines(hpc_sub$DateTime, as.numeric(hpc_sub$Sub_metering_1), col="black")
+lines(hpc_sub$DateTime, as.numeric(hpc_sub$Sub_metering_2), col="red")
+lines(hpc_sub$DateTime, as.numeric(hpc_sub$Sub_metering_3), col="blue")
+legend("topright", 
+       legend = c("Sub_metering_1", 
+                  "Sub_metering_2", 
+                  "Sub_metering_3"), 
+       col = c("black", "red", "blue"), 
+       lwd = .75, 
+       cex = .75
+)
+dev.off()
